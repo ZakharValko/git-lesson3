ce41897672df9e5ea039b75e67af4e6119801659
Zakhar <zahar.valko@gmail.com>
2021-03-05 21:03:39 +0200

    My first stage

---
+++ src/ua/alevel/git/GitPoweredApplication.java
@@ -0,0 +1,13 @@
+package ua.alevel.git;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class GitPoweredApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+
+}
